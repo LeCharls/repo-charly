169a8901cf65e711cdc2416a65c4c0a4eaa9ffde
Charly <carlosrk8@hotmail.com>
2023-01-25 13:49:50 -0500

    solution who-like-it

--- CodeWar/Who-like-it/javascript.js
+++ CodeWar/Who-like-it/javascript.js
@@ -1,29 +1,29 @@
 function likes(names) {
-    notification = ""
-    if (names.length > 1 && names.length < 3){
-        if (i + 1 !== names.length){
-            for (i = 0; i < names.length; i++);
-            notification += `${i}, `;
-        }
-        else{
-            notification += `and ${i} `;
-        }
+    let notification = ""
+    if (names.length > 1){
+        for (i = 0; i < names.length; i++){
+            if (names.length === 2){
+                notification += `${names[i]} and ${names[i + 1]} like this`
+            }
+            if (names.length === 3) {
+                notification += `${names[i]}, ${names[i+1]} and ${names[i+2]} like this`   
+            }
+            if (names.length >= 4){
+                notification += `${names[i]}, ${names[i+1]} and ${names.length - 2} others like this` 
+            }
 
-    return `${notification} like this`
-
+        return notification
+        }
     }
-    else if (names.length === 1) {
+    else if (names.length == 1) {
         return `${names[0]} likes this`
     }
-    else if (names.length > 3){
-        if (i <= 3){
-            for (i = 0; i < names.length; i++);
-            notification += `${i}, `;
-        }
-        return `${notification} and ${names.length - 3}`
-        }
-
-    else if (names.length === 0) {
+    else if (names.length == 0) {
         return "no one likes this"
     }
 }
+dos = ['Jacob', 'Alex']
+tres = ['Max', 'John', 'Mark']
+cuatro = ['Alex', 'Jacob', 'Mark', 'Max']
+console.log(likes(cuatro))
+
