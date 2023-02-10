ad60ab97a0fb7d4063147c0a913de98fa097cfe9
Charly-MAC <carlosrk8@hotmail.com>
2023-02-09 21:44:47 -0500

    app-with-react

---
+++ MS/store/src/App.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react';
+import './App.css';
+
+
+function App() {
+  const [value, updateValue] = useState([])
+  return (
+    <div className="App">
+      <section className="App-content">
+        
+        
+      </section>
+    </div>
+  );
+}
+
+export default App;
